63918e1be2c82480336b77a8ccf206750d571a64
Linda Lam <lindaqlam@gmail.com>
2020-09-15 13:26:23 -0700

    add create & delete comment feature to user schema

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router({ mergeParams: true });
 var HotTopic = require('../models/hot_topic');
+var User = require('../models/user');
 var Comment = require('../models/comment');
 var middleware = require('../middleware');
 
@@ -8,7 +9,8 @@ var middleware = require('../middleware');
 router.get('/new', middleware.isLoggedIn, function(req, res) {
 	HotTopic.findById(req.params.id, function(err, hot_topic) {
 		if (err) {
-			console.log(err);
+			req.flash('error', "You don't have permission");
+			res.redirect('/hot_topics/' + req.params.id);
 		} else {
 			res.render('comments/new', { hot_topic: hot_topic });
 		}
@@ -18,20 +20,34 @@ router.get('/new', middleware.isLoggedIn, function(req, res) {
 // COMMENTS CREATE
 router.post('/', middleware.isLoggedIn, function(req, res) {
 	HotTopic.findById(req.params.id, function(err, hot_topic) {
-		if (err) {
+		if (err || !hot_topic) {
 			console.log(err);
+			req.flash('error', 'Your comment could not be posted.');
 			res.redirect('/hot_topics');
 		} else {
 			Comment.create(req.body.comment, function(err, comment) {
 				if (err) {
 					console.log(err);
-					console.log('here');
+					req.flash('error', 'Comment could not be posted');
+					res.redirect('back');
 				} else {
 					comment.author.id = req.user._id;
 					comment.author.username = req.user.username;
 					comment.save();
 					hot_topic.comments.push(comment);
 					hot_topic.save();
+
+					User.findOne({ username: comment.author.username }, function(err, foundUser) {
+						if (err) {
+							req.flash('error', 'Comment could not be posted');
+							res.redirect('back');
+						} else {
+							foundUser.comments.push(comment);
+							foundUser.save();
+						}
+					});
+
+					req.flash('success', 'Successfully posted your comment!');
 					res.redirect('/hot_topics/' + hot_topic._id);
 				}
 			});
@@ -41,32 +57,63 @@ router.post('/', middleware.isLoggedIn, function(req, res) {
 
 //COMMENT EDIT ROUTE
 router.get('/:comment_id/edit', middleware.checkCommentAuthorization, function(req, res) {
-	Comment.findById(req.params.comment_id, function(err, foundComment) {
-		if (err) {
-			res.redirect('back');
-		} else {
-			res.render('comments/edit', { hot_topic_id: req.params.id, comment: foundComment });
+	HotTopic.findById(req.params.id, function(err, foundTopic) {
+		if (err || !foundTopic) {
+			req.flash('error', 'Hot Topic not found');
+			return res.redirect('back');
 		}
+
+		Comment.findById(req.params.comment_id, function(err, foundComment) {
+			if (err || !foundComment) {
+				req.flash('error', 'Comment not found');
+				res.redirect('back');
+			} else {
+				res.render('comments/edit', { hot_topic_id: req.params.id, comment: foundComment });
+			}
+		});
 	});
 });
 
 // COMMENT UPDATE ROUTE
 router.put('/:comment_id', middleware.checkCommentAuthorization, function(req, res) {
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
-		if (err) {
+		if (err || !updatedComment) {
+			req.flash('error', 'Your comment could not be edited.');
 			res.redirect('back');
 		} else {
-			res.redirect('/hot_topics/' + req.params.id);
+			User.findOne({ username: foundComment.author.username }, function(err, foundUser) {
+				if (err) {
+					req.flash('error', 'Your comment could not be edited.');
+					res.redirect('back');
+				} else {
+					foundUser.comment.findByIdAndUpdate({ comment_id: req.paramts.comment_id }, function(err) {});
+				}
+
+				req.flash('success', 'Successfully edited your comment!');
+				res.redirect('/hot_topics/' + req.params.id);
+			});
 		}
 	});
 });
 
 // COMMENT DELETE ROUTE
 router.delete('/:comment_id', middleware.checkCommentAuthorization, function(req, res) {
-	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+	Comment.findByIdAndRemove(req.params.comment_id, function(err, foundComment) {
 		if (err) {
+			req.flash('error', 'Your comment could not be deleted.');
 			res.redirect('back');
 		} else {
+			User.findOne({ username: foundComment.author.username }, function(err, foundUser) {
+				if (err) {
+					req.flash('error', 'Your comment could not be deleted.');
+					res.redirect('back');
+				} else {
+					foundUser.comments.pull(req.params.comment_id);
+					foundUser.save();
+				}
+			});
+
+			req.flash('success', 'Successfully deleted your comment');
 			res.redirect('/hot_topics/' + req.params.id);
 		}
 	});
